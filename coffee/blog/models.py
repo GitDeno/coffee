1b0127bbbdb8e5f0fc12534bc9e9ce1b40ac19e8
GitDeno <ddrendy@gmail.com>
2023-09-09 15:08:42 +0200

    Add: Post model

--- coffee/blog/models.py
+++ coffee/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Model for blog posts
+class Post(models.Model):
+    title = models.CharField(max_lenght=250)
+    slug = models.SlugField(max_lenght=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
+
