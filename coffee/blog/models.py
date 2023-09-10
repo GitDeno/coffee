de64505100ec63c67ebd7c9fa8ac0515e78b298a
GitDeno <ddrendy@gmail.com>
2023-09-10 21:25:39 +0200

    Feat: Comment section for posts

--- coffee/blog/models.py
+++ coffee/blog/models.py
@@ -5,12 +5,13 @@ from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
+    # Custom manager for published posts
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
-# Model for blog posts
 class Post(models.Model):
+    # Model for blog posts
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
@@ -44,3 +45,25 @@ class Post(models.Model):
             "blog:post_detail",
             args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
         )
+
+
+class Comment(models.Model):
+    # Model for comments
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["created"]
+        indexes = [
+            models.Index(fields=["created"]),
+        ]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
