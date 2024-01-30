f11ba1de3a889d7c8bfe2469474d85426c4c4548
GitDeno <ddrendy@gmail.com>
2024-01-30 16:57:50 +0100

    Refactor: Change DB to postgresql

--- coffee/coffee/settings.py
+++ coffee/coffee/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
     "taggit",  # Tags
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
@@ -81,8 +82,10 @@ WSGI_APPLICATION = "coffee.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'blog',
+        "USER": 'blog',
+        'PASSWORD': 'admin',
     }
 }
 
