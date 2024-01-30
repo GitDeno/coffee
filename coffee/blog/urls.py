c6c98bc839120d70746ab4e0eea618e80857e3ea
GitDeno <ddrendy@gmail.com>
2024-01-30 17:48:31 +0100

    Feat: simple posts search

--- coffee/blog/urls.py
+++ coffee/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = "blog"
@@ -13,4 +14,5 @@ urlpatterns = [
     ),
     path("<int:post_id>/share/", views.post_share, name="post_share"),
     path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
+    path('search/', views.post_search, name='post_search'),
 ]
